f98563f9eb366806b83749cf329538e04424da2c
Pravin Patil <pravinpatilrpa@gmail.com>
2025-09-17 09:28:50 +0000

    ok

--- Math.py
+++ Math.py
@@ -1,5 +1,5 @@
 import math
-
-print("math is easy")
 ceil_val = math.ceil(15.25)
-print( "Ceiling value of 15.25 is : ", ceil_val) 
+print( "Ceiling value of 15.25 is : ", ceil_val)
+floor_val = math.floor(15.25)
+print( "Floor value of 15.25 is : ", floor_val)
